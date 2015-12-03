9896a79ad3450e63ecdfbbf78c795910bae23088
JonnyTran <nhat.tran@mavs.uta.edu>
2015-12-03 03:19:10 -0600

    Added quicksort on different data distributions

--- experiments.py
+++ experiments.py
@@ -22,7 +22,7 @@ def partialsort_experiment(sort_algo, data_size=100000):
     x = [0,] + [int(2**exponent) for exponent in np.arange(0, 100, 1) if exponent <= np.log2(n_swaps)]
     print x
     y = []
-    data = generate_uniforms(n_uniforms=data_size, a=1, b=10000) # Pick different values maybe?
+    data = generate_uniforms(n_uniforms=data_size, a=1, b=1000000) # Pick different values maybe?
     for ps_data in generate_partialsorts(data, n_swaps=n_swaps):
         # ps_data will contain a partially sorted array (starting at fully sorted and slowly becoming less sorted,
         # until every item is out of place)
@@ -51,7 +51,7 @@ def plot_all_sorts_increasing_data(exponents_start=1, exponents_stop=6, exponent
     for sort_algo in sort_algorithms:
         plt.plot(x, y[sort_algo.func_name], label=sort_algo.func_name)
 
-    plt.title("Different Sorting Algorithms Running Time with Increasing Data Size")
+    plt.title("Different Sorting Algorithms with Increasing Data Size")
     plt.xlabel("Data Size (# of uniform integers)")
     plt.ylabel("Running Time (seconds)")
     plt.legend(bbox_to_anchor=(0.3, 1))
@@ -60,7 +60,7 @@ def plot_all_sorts_increasing_data(exponents_start=1, exponents_stop=6, exponent
 def plot_radix_vs_quicksort_small_range_data(data_size=10000000):
     sort_algorithms = [quicksort, radixsort]
 
-    x = [int(10**exponent) for exponent in np.arange(4, 0, -0.4)]
+    x = [int(10**exponent) for exponent in np.arange(9, 2, -0.7)]
     y = {}
     for sort_algo in sort_algorithms:
         y[sort_algo.func_name] = []
@@ -94,8 +94,36 @@ def plot_quick_vs_insertion_partially_sorted(data_size=1000000):
     plt.legend(bbox_to_anchor=(1, 0.3))
     plt.show()
 
+def plot_quicksort_different_data_distribution(data_size=10000, n_times=1000):
+    sort_algo = quicksort
+    data_distributions = [lambda: generate_uniforms(data_size, 1, 100000),
+                          lambda: generate_normals(data_size, 0, 5),
+                          lambda: generate_exponentials(data_size, 0.5)]
+    data_distributions_names = ["uniforms", "normals", "exponentials"]
+
+    y = {}
+    for distribution in data_distributions_names:
+        y[distribution] = []
+
+    for i, data_gen in enumerate(data_distributions):
+        print "Sorting",data_distributions_names[i]
+        for experiment in range(n_times):
+            running_time = allsort_experiment_avg_time(sort_algo, data_gen)
+            y[data_distributions_names[i]].append(running_time)
+
+    for distribution in data_distributions_names:
+        # plt.hist(gaussian_numbers, bins=20, histtype='stepfilled', normed=True, color='b', label='Gaussian')
+        plt.hist(y[distribution], bins=200, histtype='step', label=distribution)
+
+    plt.title("QuickSort on Different Data Distributions (%d Ints)" % data_size)
+    plt.xlabel("Running Time (seconds)")
+    plt.ylabel("Frequency")
+    plt.legend()
+    plt.show()
+
 
 if __name__ == '__main__':
     # plot_all_sorts_increasing_data()
     # plot_radix_vs_quicksort_small_range_data()
-    plot_quick_vs_insertion_partially_sorted()
+    # plot_quick_vs_insertion_partially_sorted()
+    plot_quicksort_different_data_distribution()
